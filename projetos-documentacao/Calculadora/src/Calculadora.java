be2d68a1040e59b5eff45a8cfe2847103447e3ea
Christopher.Martins <christopher.c.martins15@gmail.com>
2024-08-27 11:08:39 -0300

    Adição do projeto Calculadora

---
+++ projetos-documentacao/Calculadora/src/Calculadora.java
@@ -0,0 +1,27 @@
+/**
+ * <h1>Calculadora<h1>
+ * A Calculadora realiza operações metematicas entre numeros inteiros
+ * <p>
+ * <B>Note:<b/> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor.
+ *
+ @author Gleyson Sampaio
+ @version 1.0
+ @since 01/01/2022
+*/
+
+public class Calculadora {
+
+    /** 
+     * Este metodo é utilizado para somar dois numeros inteiros
+     * @param numeroUm este é o primeiro parametro de metodo
+     * @param numeroDois este é o segundo parametro de metodo
+     * @return int o resultado deste numero é a soma dos dois numeros
+    */
+
+     public int somar (int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+     }
+}
+
+//Obs Chris.: Achei isso irado...
