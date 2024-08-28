7779660f8ccb98846acd65665c8f72e745977d4c
Christopher.Martins <christopher.c.martins15@gmail.com>
2024-08-28 14:00:43 -0300

    Adição das estruturas condicionais

--- java-terminal/AboutMe2/utilizando-classe-scanner/src/AboutMeScanner.java
+++ java-terminal/AboutMe2/utilizando-classe-scanner/src/AboutMeScanner.java
@@ -1,6 +1,27 @@
 import java.util.Scanner;
 
+/**
+ * <h1>Requisição de Dados<h1>
+ * <p>Este formulario captura dados do usuario utilizando a classe Scanner.<p>
+ * <B>Note:<b/> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor.
+ *
+ @author Christopher Martins
+ @version 1.0
+ @since 27/08/2024
+*/
+
 public class AboutMeScanner {
+
+        /** 
+     * Este metodo é utilizado para capturar os dados do usuario
+     * @param nome este é o primeiro parametro de metodo
+     * @param sobreNome este é o segundo parametro de metodo
+     * @param idade este é o terceiro parametro de metodo
+     * @param altura este é o quarto parametro de metodo
+     * @return o sistema irá printar os dados junto a mensagem definida na classe.
+    */
+
     public static void main(String[] args) throws Exception {
 
         //Criando o objeto Scanner.
@@ -24,9 +45,5 @@ public class AboutMeScanner {
          System.out.println("Olá, me chamo" + " " + nome + " " + segundoNome + "!");
          System.out.println("Tenho" + " " + idade + " " + "Anos.");
          System.out.println("Minha altura é:" + " " + altura + "cm");
-  
-          
-    
-
     }
 }
