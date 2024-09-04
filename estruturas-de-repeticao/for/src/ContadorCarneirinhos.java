06913ffe860085836e7a4fa5697ccd70f93e8ce2
Christopher.Martins <christopher.c.martins15@gmail.com>
2024-09-04 14:51:51 -0300

    Criação do contador de carneirinhos utilizando o laço de repetição FOR

---
+++ estruturas-de-repeticao/for/src/ContadorCarneirinhos.java
@@ -0,0 +1,11 @@
+public class ContadorCarneirinhos {
+    public static void main(String[] args) throws Exception {
+        
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++){
+
+        System.out.println("Contando Carneirinhos: " + carneirinhos);
+        }
+
+        System.out.println("Caiu no sono zzz...");
+    }
+}
