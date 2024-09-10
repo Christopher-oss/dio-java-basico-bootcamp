b52ed94dcd54ebc21ae1547ff0e870e31705098b
Christopher.Martins <christopher.c.martins15@gmail.com>
2024-09-10 13:03:53 -0300

    Adição de exemplos
    Complemento dos exemplos vistos em aula.

---
+++ estruturas-de-repeticao/for-each/src/exemploForEach.java
@@ -0,0 +1,10 @@
+public class exemploForEach {
+    public static void main(String[] args) throws Exception {
+        
+        String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
+    
+        for (String aluno : alunos ){
+            System.out.println("O nome do aluno é: " + aluno);
+        }
+    }
+}
